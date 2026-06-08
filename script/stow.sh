ac6ddc6b3502e8174754fa1528d86d0c84ddfe68
Shaymin <2767272116@qq.com>
2026-06-08 21:27:58 +0800

    update stow opencode

--- script/stow.sh
+++ script/stow.sh
@@ -21,8 +21,17 @@ packages=(
     yazi
 )
 
+# stow 在目标位置已存在文件时会失败
+# .bashrc 若为普通文件则备份，若为错误软链接则删除
+if [ -f "$HOME/.bashrc" ] && [ ! -L "$HOME/.bashrc" ]; then
+    mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+elif [ -L "$HOME/.bashrc" ]; then
+    rm "$HOME/.bashrc"
+fi
+
+# 批量执行stow恢复config
 stow --dir "$HOME/dotfiles/stow" -t "$HOME" "${packages[@]}"
 
-# root的配置
+# root的配置，未研究明白，下次研究下
 
 # sudo stow --dir "$HOME/dotfiles/system" -t / keyd
